1faadc308a413a8efbbbb1b755505f6b544300aa
prakashindukuri <160770413+prakash-2736@users.noreply.github.com>
2026-05-24 23:59:32 +0530

    Comment out Pascal's Triangle generation code
    Commented out the implementation of Pascal's Triangle generation.

--- Array/118. Pascal's Triangle.cpp
+++ Array/118. Pascal's Triangle.cpp
@@ -20,7 +20,19 @@ public:
         }
         return ans;
 
+// vector<vector<int>> triangle;
 
+//         for (int i = 0; i < numRows; ++i) {
+//             vector<int> row(i + 1, 1);
+
+//             for (int j = 1; j < i; ++j) {
+//                 row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+//             }
+
+//             triangle.push_back(row);
+//         }
+
+//         return triangle;
 
       
     }
