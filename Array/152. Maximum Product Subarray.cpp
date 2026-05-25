ffc95da4627b0a18b78c41e5a628ecf6ef2eb7aa
prakashindukuri <160770413+prakash-2736@users.noreply.github.com>
2026-05-25 22:03:30 +0530

    Comment out maximum product subarray logic
    Comment out the implementation of the maximum product subarray algorithm.

--- Array/152. Maximum Product Subarray.cpp
+++ Array/152. Maximum Product Subarray.cpp
@@ -21,5 +21,24 @@ public:
 
         // Return the final answer
         return ans;
+
+
+
+
+        // int currMax = nums[0];
+        // int currMin = nums[0];
+        // int ans = nums[0];
+
+        // for(int i = 1; i < nums.size(); i++) {
+        //     if(nums[i] < 0)                                   // for overflow 
+        //         swap(currMax, currMin);
+
+        //     currMax = max(nums[i], currMax * nums[i]);
+        //     currMin = min(nums[i], currMin * nums[i]);
+
+        //     ans = max(ans, currMax);
+        // }
+
+        // return ans;
     }
 };
