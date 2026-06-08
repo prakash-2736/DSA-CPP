a9b333e143d0fe125bceba25eb5c7f5a562470d9
prakashindukuri <160770413+prakash-2736@users.noreply.github.com>
2026-06-08 16:33:53 +0530

    Update 34. Find First and Last Position of Element in Sorted Array.cpp

--- BINARY_SEARCH/34. Find First and Last Position of Element in Sorted Array.cpp
+++ BINARY_SEARCH/34. Find First and Last Position of Element in Sorted Array.cpp
@@ -1,48 +1,51 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int lb = lowerBound(nums,nums.size(), target);
-        if(lb == nums.size() || nums[lb] != target ) return {-1,-1};
-        int up = upperBound(nums,nums.size(),target);
-        return {lb,up-1};
+        // int lb = lowerBound(nums,nums.size(), target);
 
+        // int up = upperBound(nums,nums.size(),target);
+
+        int lb = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        if (lb == nums.size() || nums[lb] != target)
+            return {-1, -1};
+        int up = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
+        return {lb, up - 1};
     }
 
+    // int upperBound(vector<int> &nums, int n, int target) {
+    //     int low = 0, high = n - 1;
+    //     int ans = n;
 
-int upperBound(vector<int> &nums, int n, int target) {
-    int low = 0, high = n - 1;
-    int ans = n;
+    //     while (low <= high) {
+    //         int mid = (low + high) / 2;
 
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        
-        if (nums[mid] > target) {
-            ans = mid;
-            
-            high = mid - 1;
-        }
-        else {
-            low = mid + 1; 
-        }
-    }
-    return ans;
-}
-    int lowerBound(vector<int> &nums, int n, int target) {
-    int low = 0, high = n - 1;
-    int ans = n;
+    //         if (nums[mid] > target) {
+    //             ans = mid;
 
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        
-        if (nums[mid] >= target) {
-            ans = mid;
-            
-            high = mid - 1;
-        }
-        else {
-            low = mid + 1; 
-        }
-    }
-    return ans;
-}
+    //             high = mid - 1;
+    //         }
+    //         else {
+    //             low = mid + 1;
+    //         }
+    //     }
+    //     return ans;
+    // }
+    //     int lowerBound(vector<int> &nums, int n, int target) {
+    //     int low = 0, high = n - 1;
+    //     int ans = n;
+
+    //     while (low <= high) {
+    //         int mid = (low + high) / 2;
+
+    //         if (nums[mid] >= target) {
+    //             ans = mid;
+
+    //             high = mid - 1;
+    //         }
+    //         else {
+    //             low = mid + 1;
+    //         }
+    //     }
+    //     return ans;
+    // }
 };
