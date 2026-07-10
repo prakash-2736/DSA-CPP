2dc6f6291b42830a04d6ff9e8b49155cad149326
prakashindukuri <160770413+prakash-2736@users.noreply.github.com>
2026-07-10 14:58:25 +0530

    Add solution for maximum level sum of binary tree
    Implement a solution to find the maximum level sum of a binary tree.

---
+++ Binary_tree/1161. Maximum Level Sum of a Binary Tree.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        int ans;
+        if (!root) {
+            return ans;
+        }
+
+        queue<TreeNode*> q;
+        q.push(root);
+        int maxi = INT_MIN;
+        int c = 0;
+
+        while (!q.empty()) {
+
+            int n = q.size();
+            c++;
+            int sum = 0;
+
+            for (int i = 0; i < n; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                sum += node->val;
+
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+
+            if (sum > maxi) {
+                maxi = sum;
+                ans = c;
+            }
+        }
+
+        return ans;
+    }
+};
