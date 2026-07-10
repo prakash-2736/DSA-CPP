59400bae97e6bc79716d2795191dbd51f7a4ddc0
prakashindukuri <160770413+prakash-2736@users.noreply.github.com>
2026-07-10 15:30:10 +0530

    Change maxDepth to iterative BFS implementation
    Refactor maxDepth function to use iterative approach with queue.

--- Binary_tree/104. Maximum Depth of Binary Tree.cpp
+++ Binary_tree/104. Maximum Depth of Binary Tree.cpp
@@ -6,19 +6,48 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if(!root){
+        // if(!root){
+        //     return 0;
+        // }
+        // int left = maxDepth(root->left);
+        // int right = maxDepth(root->right);
+
+        // return max(left,right) + 1  ;
+
+        if (root == NULL)
             return 0;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        int depth = 0;
+
+        while (!q.empty()) {
+
+            int n = q.size();
+
+            while (n--) {
+
+                TreeNode* node = q.front();
+                q.pop();
+
+                if (node->left)
+                    q.push(node->left);
+
+                if (node->right)
+                    q.push(node->right);
+            }
+
+            depth++;
         }
-        int left = maxDepth(root->left);
-        int right = maxDepth(root->right);
 
-
-        return max(left,right) + 1  ;
+        return depth;
     }
 };
