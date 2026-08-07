5b8c65f2d5a35d0a9656e7a7a2eab91bffe2359a
prakashindukuri <160770413+prakash-2736@users.noreply.github.com>
2026-08-07 22:53:03 +0530

    Implement solution for longest palindromic substring

---
+++ string/5. Longest Palindromic Substring.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    int expandAroundCenter(const string& s, int left, int right) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+public:
+    string longestPalindrome(string s) {
+         if (s.empty()) return "";
+        
+        int start = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            
+            int len2 = expandAroundCenter(s, i, i + 1);
+            
+            int len = max(len1, len2);
+            
+            if (len > maxLength) {
+                maxLength = len;
+                start = i - (len - 1) / 2;
+            }
+        }
+
+        return s.substr(start, maxLength);
+    }
+};
