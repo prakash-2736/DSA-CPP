8af7b2607d4a51dc246546419a5ed003fd5b2b3a
prakashindukuri <160770413+prakash-2736@users.noreply.github.com>
2026-08-22 14:33:30 +0530

    Update 70. Climbing Stairs.cpp

--- dynamic_programming/70. Climbing Stairs.cpp
+++ dynamic_programming/70. Climbing Stairs.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-    int fib(int n, vector<int>& dp) {
-        if (n == 1)
-            return 1;
-        if (n == 2)
-            return 2;
-        if (dp[n] != -1) {
+    // int fib(int n, vector<int>& dp) {
+    //     if (n == 1)
+    //         return 1;
+    //     if (n == 2)
+    //         return 2;
+    //     if (dp[n] != -1) {
 
-            return dp[n];
-        }
-        dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
-        return dp[n];
-    }
+    //         return dp[n];
+    //     }
+    //     dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
+    //     return dp[n];
+    // }
     int climbStairs(int n) {
-        if (n == 1)
-            return 1;
-        if (n == 2)
-            return 2;
+        // if (n == 1)
+        //     return 1;
+        // if (n == 2)
+        //     return 2;
+
+        // vector<int> dp(n + 1, -1);
+        // int a = fib(n, dp);
+
+        // return a;
 
         vector<int> dp(n + 1, -1);
-        int a = fib(n, dp);
 
-        return a;
+        dp[0] = 1;
+        dp[1] = 1;
+
+        //tabulation 
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
 };
